188c580cc1226bbb4f9a95b241318b9c68dc5394
Sara Basso <sbasso@cs.ucsb.edu>
2026-05-21 14:58:29 -0700

    Second version of pa02

--- movies.cpp
+++ movies.cpp
@@ -7,21 +7,39 @@ void printMovies(map<string, float> movies){
         cout << it->first << ", " << it->second << endl;
     }
 }
+bool mySort(pair<string, float> a, pair<string, float> b){
+    if (a.second == b.second){
+        return a.first < b.first;
+    }    
+    return a.second > b.second;
+    }
+void printMoviesDecreasing(map<string, float> hasPre){
+    vector<pair<string,float>> v(hasPre.begin(), hasPre.end());
+    sort(v.begin(),v.end(), mySort);
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i].first << ", " << v[i].second << endl;
+    }
+}
+
 
 void checkPrefixes(vector<string> prefixes, map<string, float> movies){
+    map<string, float> hasPre;
     for (int i = 0; i < prefixes.size(); i ++){
+        hasPre.clear();
         string bestName = "";
         float bestRating = 0;
-
         for (auto it = movies.begin(); it != movies.end();it++){
             if(it->first.substr(0,prefixes[i].length()) == prefixes[i]){
                 if (it->second > bestRating){
                     bestRating = it->second;
                     bestName = it->first;
                 }
+                hasPre[it->first] = it->second;
             }
         }
-        if (bestName.empty()){
+        printMoviesDecreasing(hasPre);
+
+        if (hasPre.empty()){
             cout << "No movies found with prefix " << prefixes[i];
         }
         else {
--- movies.h
+++ movies.h
@@ -7,4 +7,8 @@ using namespace std;
 
 void printMovies(map<string, float> movies);
 
+void printMoviesDecreasing(map<string, float> hasPre);
+
+bool Sort(pair<string, int> a, pair<string, int> b);
+
 void checkPrefixes(vector<string> prefixes, map<string, float> movies);
